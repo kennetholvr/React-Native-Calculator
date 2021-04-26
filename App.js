9b3db706db9f4f98ab3d3b73065b1d2eb4c668ed
kthomas80 <kthomas80@dxc.com>
2021-04-26 14:44:52 -0500

    adds calc logic and diplay component

--- App.js
+++ App.js
@@ -1,43 +1,140 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-import NumberButton from './components/numberButton.js'
+import NumberButton from "./components/numberButton";
 
 const buttons = [
-  ['Clear', 'Del'],
-  [7 ,8 ,9, '%'],
-  [4 ,5 ,6, '*'],
-  [1 ,2 ,3, '-'],
-  [0 , '.', '=', '+'],
+  ["CLEAR", "DEL"],
+  ["7", "8", "9", "/"],
+  ["4", "5", "6", "x"],
+  ["1", "2", "3", "-"],
+  ["0", ".", "=", "+"],
+];
 
-]
-
-export default function App() {
-  
-  const renderButtons = () => {
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      displayValue: "0",
+      operator: null,
+      firstValue: "",
+      secondValue: "",
+      nextValue: false,
+    };
+  }
+  renderButtons() {
     let layouts = buttons.map((buttonRows, index) => {
       let rowItem = buttonRows.map((buttonItems, buttonIndex) => {
-        return <NumberButton 
-        value={buttonItems}
-        handleOnPress={() => {}}
-        key={'btn-' + buttonIndex }/>
-      })
-      return <View style={styles.inputRow} key={'row-' + index}>{rowItem}</View>
-    })
-    return layouts
+        return (
+          <NumberButton
+            value={buttonItems}
+            handleOnPress={this.handleInput.bind(this, buttonItems)}
+            key={"btn-" + buttonIndex}
+          />
+        );
+      });
+      return (
+        <View style={styles.inputRow} key={"row-" + index}>
+          {rowItem}
+        </View>
+      );
+    });
+
+    return layouts;
+  }
+
+  handleInput = (input) => {
+    const {
+      displayValue,
+      operator,
+      firstValue,
+      secondValue,
+      nextValue,
+    } = this.state;
+    switch (input) {
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        this.setState({
+          displayValue: displayValue === "0" ? input : displayValue + input,
+        });
+
+        if (!nextValue) {
+          this.setState({
+            firstValue: firstValue + input,
+          });
+        } else {
+          this.setState({
+            secondValue: secondValue + input,
+          });
+        }
+        break;
+      case "+":
+      case "-":
+      case "x":
+      case "/":
+        this.setState({
+          operator: input,
+          displayValue:
+            (operator !== null
+              ? displayValue.substr(0, displayValue.length - 1)
+              : displayValue) + input,
+          nextValue: true,
+        });
+        break;
+      case "=":
+        let formatOperator = operator == "x" ? "*" : operator;
+        let result = eval(firstValue + formatOperator + secondValue);
+        this.setState({
+          displayValue: result % 1 === 0 ? result : result.toFixed(2),
+          firstValue: result % 1 === 0 ? result : result.toFixed(2),
+          secondValue: "",
+          operator: null,
+          nextValue: false,
+        });
+        break;
+      case "CLEAR":
+        this.setState({
+          operator: null,
+          displayValue: "0",
+        });
+        break;
+      case "DEL":
+        let string = displayValue.toString();
+        let length = string.length;
+        this.setState({
+          displayValue: length == 1 ? "0" : displayValue.slice(0, -1),
+          firstValue: length == 1 ? "" : displayValue.slice(0, -1),
+        });
+        break;
+      case ".":
+        let dot = displayValue.toString().slice(-1);
+        this.setState({
+          displayValue: dot !== "." ? displayValue + input : displayValue,
+        });
+        break;
+    }
   };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.resultContainer}>
-        <Text style={styles.resultText}>0</Text>
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.resultContainer}>
+          <Text style={styles.resultText}>{this.state.displayValue}</Text>
+        </View>
+        <View style={styles.inputContainer}>
+          {this.renderButtons()}
+        </View>
       </View>
-      <View style={styles.buttonContainer}>
-            {renderButtons()}
-      </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -46,22 +143,22 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
     flex: 2,
-    justifyContent: 'center',
-    backgroundColor: '#1E1240'
+    justifyContent: "center",
+    backgroundColor: "#446418",
   },
-  buttonContainer: {
+  inputContainer: {
     flex: 8,
-    backgroundColor: '#3D0075'
+    backgroundColor: "#7d9b5d",
   },
   resultText: {
-    color: 'white',
+    color: "white",
     fontSize: 80,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     padding: 20,
-    textAlign: 'right'
+    textAlign: "right",
   },
-  inputRow:{
+  inputRow: {
     flex: 1,
-    flexDirection: 'row'
-  }
+    flexDirection: "row",
+  },
 });
